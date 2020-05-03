fd82c3a1156517065c9b8e1a5716b3f9a672ac3f
Andżej <trader1999@o2.pl>
2020-05-03 10:58:08 +0200

    add UI.Blazor which is not attached to backend yet

---
+++ UI.Blazor/Data/Candidate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI.Blazor.Data
+{
+    public class Candidate
+    {
+        public int Value { get; set; }
+        public bool IsValid { get; set; }
+    }
+}
