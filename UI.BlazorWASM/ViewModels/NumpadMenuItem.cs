017f7169fcae0ad0fd113cd6f7fe33ac357af519
Andżej <trader1999@o2.pl>
2020-05-14 11:02:38 +0200

    refact

--- UI.BlazorWASM/ViewModels/NumpadMenuItem.cs
+++ UI.BlazorWASM/ViewModels/NumpadMenuItem.cs
@@ -10,12 +10,27 @@ namespace UI.BlazorWASM.ViewModels
 {
     public class NumpadMenuItem : INumpadMenuItem
     {
-
+        private IFilter _filter;
+        private EventCallback<IFilter> _filterChangedEvent;
+        private Action<IFilter> _filterChangedAction;
+        private Action _executeAction;
+        private Func<bool> _canExecutePredicate;
         private Func<bool> _isDimmedPredicate;
-        public NumpadMenuItem SetIsDimmedPredicate(Func<bool> isDimmedPredicate)
+        private IClickableAction _clickableAction;
+        private EventCallback<IClickableAction> _clickableActionChangedEvent;
+        private Action<IClickableAction> _clickableActionChangedAction;
+        
+        public bool IsDimmed => _isDimmedPredicate?.Invoke() ?? false;
+        public bool IsSelectable { get; private set; } = true;
+        public string Label { get; private set; }
+        public bool CanExecute => _canExecutePredicate?.Invoke() ?? true;
+        public async Task Execute()
         {
-            _isDimmedPredicate = isDimmedPredicate;
-            return this;
+            _executeAction?.Invoke();
+            await _clickableActionChangedEvent.InvokeAsync(_clickableAction);
+            _clickableActionChangedAction?.Invoke(_clickableAction);
+            await _filterChangedEvent.InvokeAsync(_filter);
+            _filterChangedAction?.Invoke(_filter);
         }
 
         public NumpadMenuItem SetIsSelectable(bool isSelectable)
@@ -30,50 +45,22 @@ namespace UI.BlazorWASM.ViewModels
             return this;
         }
 
-        private Func<bool> _canExecutePredicate;
         public NumpadMenuItem WithCanExecute(Func<bool> canExecutePredicate)
         {
             _canExecutePredicate = canExecutePredicate;
             return this;
         }
-
-        public bool IsDimmed => _isDimmedPredicate?.Invoke() ?? false;
-
-        public bool IsSelectable { get; private set; } = true;
-
-        public string Label { get; private set; }
-
-        public bool CanExecute => _canExecutePredicate?.Invoke() ?? true;
-
-
-        private IClickableAction _clickableAction;
-        private EventCallback<IClickableAction> _clickableActionChangedEvent;
-
-        public NumpadMenuItem WithChangeAction(EventCallback<IClickableAction> clickableActionChanged, IClickableAction action)
+        public NumpadMenuItem WithExecute(Action executeAction)
         {
-            _clickableAction = action;
-            _clickableActionChangedEvent = clickableActionChanged;
+            _executeAction = executeAction;
             return this;
         }
-
-        private Action<IClickableAction> _clickableActionChangedAction;
-        public NumpadMenuItem WithChangeAction(Action<IClickableAction> clickableActionChanged, IClickableAction action)
-        {
-            _clickableAction = action;
-            _clickableActionChangedAction = clickableActionChanged;
-            return this;
-        }
-
-        private IFilter _filter;
-        private EventCallback<IFilter> _filterChangedEvent;
         public NumpadMenuItem WithChangeFilter(EventCallback<IFilter> filterChanged, IFilter filter)
         {
             _filter = filter;
             _filterChangedEvent = filterChanged;
             return this;
         }
-
-        private Action<IFilter> _filterChangedAction;
         public NumpadMenuItem WithChangeFilter(Action<IFilter> filterChanged, IFilter filter)
         {
             _filter = filter;
@@ -81,21 +68,23 @@ namespace UI.BlazorWASM.ViewModels
             return this;
         }
 
-        private Action _executeAction;
-        public NumpadMenuItem WithExecute(Action executeAction)
+        public NumpadMenuItem WithChangeAction(EventCallback<IClickableAction> clickableActionChanged, IClickableAction action)
         {
-            _executeAction = executeAction;
+            _clickableAction = action;
+            _clickableActionChangedEvent = clickableActionChanged;
+            return this;
+        }
+        public NumpadMenuItem WithChangeAction(Action<IClickableAction> clickableActionChanged, IClickableAction action)
+        {
+            _clickableAction = action;
+            _clickableActionChangedAction = clickableActionChanged;
+            return this;
+        }
+        public NumpadMenuItem WithIsDimmed(Func<bool> isDimmedPredicate)
+        {
+            _isDimmedPredicate = isDimmedPredicate;
             return this;
         }
 
-        public async Task Execute()
-        {
-            _executeAction?.Invoke();
-            await _clickableActionChangedEvent.InvokeAsync(_clickableAction);
-            _clickableActionChangedAction?.Invoke(_clickableAction);
-            await _filterChangedEvent.InvokeAsync(_filter);
-            _filterChangedAction?.Invoke(_filter);
-            
-        }
     }
 }
