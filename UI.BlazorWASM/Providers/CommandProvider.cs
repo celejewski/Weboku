7e27bd7239f65316bff6a18d12cf2414e25ca5b6
Andżej <trader1999@o2.pl>
2020-05-17 09:46:11 +0200

    add ctr+f hotkey for find all candidates

--- UI.BlazorWASM/Providers/CommandProvider.cs
+++ UI.BlazorWASM/Providers/CommandProvider.cs
@@ -13,12 +13,14 @@ namespace UI.BlazorWASM.Providers
         private readonly IGridGenerator _gridGenerator;
         private readonly IGridHistoryManager _gridHistoryManager;
         private readonly ISudokuProvider _sudokuProvider;
+        private readonly IHotkeyProvider _hotkeyProvider;
 
-        public CommandProvider(IGridGenerator gridGenerator, IGridHistoryManager gridHistoryManager, ISudokuProvider sudokuProvider)
+        public CommandProvider(IGridGenerator gridGenerator, IGridHistoryManager gridHistoryManager, ISudokuProvider sudokuProvider, HotkeyProvider hotkeyProvider)
         {
             _gridGenerator = gridGenerator;
             _gridHistoryManager = gridHistoryManager;
             _sudokuProvider = sudokuProvider;
+            _hotkeyProvider = hotkeyProvider;
         }
         public ICommand StartNewGame(string difficulty)
         {
@@ -27,7 +29,9 @@ namespace UI.BlazorWASM.Providers
 
         public ICommand FindAllCandidates()
         {
-            return new FindAllCandidatesCommand(_sudokuProvider, _gridHistoryManager);
+            var command = new FindAllCandidatesCommand(_sudokuProvider, _gridHistoryManager);
+            _hotkeyProvider.Register(new Hotkey { Command = command, Key = "f", Ctrl = true });
+            return command;
         }
 
         public ICommand Restart()
