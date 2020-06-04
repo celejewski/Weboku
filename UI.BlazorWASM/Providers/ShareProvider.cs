f1bcd7785ddb521c916d421325edce311b3a213d
Andżej <trader1999@o2.pl>
2020-06-04 10:35:49 +0200

    add share layer

--- UI.BlazorWASM/Providers/ShareProvider.cs
+++ UI.BlazorWASM/Providers/ShareProvider.cs
@@ -1,58 +1,83 @@
 ﻿using Core.Converters;
 using Core.Data;
 using System;
-using System.Threading.Tasks;
 using UI.BlazorWASM.Enums;
 
-namespace UI.BlazorWASM.Providers 
+namespace UI.BlazorWASM.Providers
 {
     public class ShareProvider : IProvider
     {
-        public string Converted { get => _converter.ToText(Grid); }
+        private bool _dirty;
         private readonly IGridConverter _converter;
+        private Func<ICell, int> _cellToValue;
         public event Action OnChanged;
         private readonly ISudokuProvider _sudokuProvider;
-        public readonly IGrid Grid;
+
+        private string _converted;
+        public string Converted 
+        { 
+            get
+            {
+                if (_dirty)
+                {
+                    Update();
+                }
+                return _converted;
+            } 
+            private set => _converted = value;
+        }
+
+
+        private readonly IGrid _grid;
+        public IGrid Grid
+        {
+            get 
+            {
+                if (_dirty)
+                {
+                    Update();
+                }
+                return _grid;
+            }
+        }
         public SharedFields SharedFields { get; set; } = SharedFields.GivensAndInputs;
 
         public ShareProvider(ISudokuProvider sudokuProvider, HodokuGridConverter hodokuGridConverter)
         {
             _sudokuProvider = sudokuProvider;
             _converter = hodokuGridConverter;
-            Grid = sudokuProvider.GetGridClone();
+            _grid = sudokuProvider.GetGridClone();
+            SelectGivenOnly();
+            sudokuProvider.OnValueOrCandidatesChanged += () => _dirty = true;
         }
 
         private void Update()
-        {
-            OnChanged?.Invoke();
-        }
-
-        public Task SelectGivenOnly()
-        {
-            SharedFields = SharedFields.Givens;
-            Convert(cell => cell.IsGiven ? cell.Input.Value : 0);
-            Update();
-            return Task.CompletedTask;
-        }
-
-        public Task SelectGivenAndUserInput()
-        {
-            SharedFields = SharedFields.GivensAndInputs;
-            Convert(cell => cell.Input.Value);
-            Update();
-            return Task.CompletedTask;
-        }
-
-        private void Convert(Func<ICell, int> CellToValue)
         {
             for( int x = 0; x < 9; x++ )
             {
                 for( int y = 0; y < 9; y++ )
                 {
                     var source = _sudokuProvider.Cells[x, y];
-                    Grid.SetValue(x, y, CellToValue(source));
+                    _grid.SetValue(x, y, _cellToValue(source));
                 }
             }
+            Converted = _converter.ToText(_grid);
+            _dirty = false;
+            OnChanged?.Invoke();
+        }
+
+        public void SelectGivenOnly()
+        {
+            SharedFields = SharedFields.Givens;
+            _cellToValue = cell => cell.IsGiven ? cell.Input.Value : 0;
+            Update();
+        }
+
+        public void SelectGivenAndUserInput()
+        {
+            SharedFields = SharedFields.GivensAndInputs;
+            _cellToValue = cell => cell.Input.Value;
+            Update();
         }
     }
 }
