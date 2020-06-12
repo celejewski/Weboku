d2fc87836ee7c7cea2abaf90083883ee6b9618d6
Andżej <trader1999@o2.pl>
2020-06-12 23:36:48 +0200

    remove comments

--- UI.BlazorWASM/Managers/GridHistoryManager.cs
+++ UI.BlazorWASM/Managers/GridHistoryManager.cs
@@ -62,58 +62,5 @@ namespace UI.BlazorWASM.Managers
                 Changed();
             }
         }
-
-        //private readonly Stack<IGrid> _previousStates = new Stack<IGrid>();
-        //private readonly Stack<IGrid> _nextStates = new Stack<IGrid>();
-
-        //public event Action OnChanged;
-
-        //public bool CanUndo { get => _previousStates.Count > 0; }
-        //public bool CanRedo { get => _nextStates.Count > 0; }
-
-        //public void Save()
-        //{
-        //    //_previousStates.Push(_sudokuProvider.GetGridClone());
-        //    _nextStates.Clear();
-        //    OnChanged?.Invoke();
-        //}
-
-        //public void Undo()
-        //{
-        //    if( CanUndo )
-        //    {
-        //        var current = _sudokuProvider.GetGridClone();
-        //        _nextStates.Push(current);
-
-        //        var previous = _previousStates.Pop();
-        //        _sudokuProvider.AssignFrom(previous);
-        //        OnChanged?.Invoke();
-        //    }
-        //}
-
-        //public void Redo()
-        //{
-        //    if( CanRedo )
-        //    {
-        //        var current = _sudokuProvider.GetGridClone();
-        //        _previousStates.Push(current);
-
-        //        var next = _nextStates.Pop();
-        //        _sudokuProvider.AssignFrom(next);
-        //        OnChanged?.Invoke();
-        //    }
-        //}
-
-        //public void ClearUndo()
-        //{
-        //    _previousStates.Clear();
-        //    OnChanged?.Invoke();
-        //}
-
-        //public void ClearRedo()
-        //{
-        //    _nextStates.Clear();
-        //    OnChanged?.Invoke();
-        //}
     }
 }
