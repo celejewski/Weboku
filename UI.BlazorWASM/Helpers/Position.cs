ce949bd228357dc751d51d11a2098a8eee9049a1
Andżej <trader1999@o2.pl>
2020-06-13 22:06:15 +0200

    add house

--- UI.BlazorWASM/Helpers/Position.cs
+++ UI.BlazorWASM/Helpers/Position.cs
@@ -2,15 +2,15 @@
 {
     public readonly struct Position
     {
+        readonly public int X;
+        readonly public int Y;
+        public int House => (y / 3) * 3 + (x / 3);
+
         public Position(int x, int y)
         {
             X = x;
             Y = y;
         }
-
-        readonly public int X;
-        readonly public int Y;
-
         public override string ToString() => $"r{Y+1}c{X+1}";
     }
 }
