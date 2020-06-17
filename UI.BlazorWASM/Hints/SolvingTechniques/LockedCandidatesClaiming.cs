bea394dd703c5e55a8107e258636132ecc658f32
Andżej <trader1999@o2.pl>
2020-06-17 13:40:53 +0200

    add explanation

--- UI.BlazorWASM/Hints/SolvingTechniques/LockedCandidatesClaiming.cs
+++ UI.BlazorWASM/Hints/SolvingTechniques/LockedCandidatesClaiming.cs
@@ -1,4 +1,5 @@
 ﻿using Core.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using UI.BlazorWASM.Helpers;
@@ -9,14 +10,29 @@ namespace UI.BlazorWASM.Hints.SolvingTechniques
     {
         private readonly InputValue _inputValue;
         private readonly IEnumerable<Position> _positionsToRemoveCandidate;
+        private IEnumerable<Position> _positionsWithLegalCandidate;
+        private IEnumerable<Position> _positionsWithIllegalCandidate;
+        private Position Position => _positionsWithLegalCandidate.First();
         private readonly House _house;
+        private string _houseFormatted;
+        private string _blockFormatted;
 
         public LockedCandidatesClaiming(InputValue inputValue, IEnumerable<Position> positionsToRemoveCandidate, House house)
-            :base("Locked Candidates - Claiming")
+            : base("Locked Candidates - Claiming")
         {
             _inputValue = inputValue;
             _positionsToRemoveCandidate = positionsToRemoveCandidate;
             _house = house;
+
+            _explanationSteps.AddRange(new Action<Displayer, Informer>[]
+            {
+                Explain1,
+                Explain2,
+                Explain3,
+                Explain4,
+                Explain5,
+            }
+                );
         }
 
         public override bool CanExecute(Informer informer)
@@ -24,28 +40,86 @@ namespace UI.BlazorWASM.Hints.SolvingTechniques
             return _positionsToRemoveCandidate.Any(pos => informer.HasCandidate(pos, _inputValue));
         }
 
+        private void SetupDisplay(Displayer displayer, Informer informer)
+        {
+            displayer.Clear();
+            displayer.SetTitle(_title);
+            var positionsWithCandidate = informer.GetPositionsWithCandidate(House.Block, _positionsToRemoveCandidate.First(), _inputValue);
+            _positionsWithLegalCandidate = positionsWithCandidate.Except(_positionsToRemoveCandidate);
+            _houseFormatted = Displayer.Format(_house, Position);
+            _blockFormatted = $"block { Position.Block + 1 }";
+            _positionsWithIllegalCandidate = GetPositiosnWithIllegalCandidate(informer);
+        }
+
         public override void DisplaySolution(Displayer displayer, Informer informer)
         {
-            var block = _positionsToRemoveCandidate.First().Block;
-            var house = Displayer.Format(_house);
-            displayer.SetTitle(_title);
-            displayer.SetDescription($"The only place in {house} where {_inputValue:D} can appear is in block {block+1}. " +
-                $"So the value of {_inputValue:D} cannot appear anywhere else in the block of {block+1} because then the {house} would have no value of {_inputValue:D} anywhere.");
+            SetupDisplay(displayer, informer);
 
-            var positionsWithCandidate = informer.GetPositionsWithCandidate(House.Block, _positionsToRemoveCandidate.First(), _inputValue);
-            var positionsWithLegalCandidate = positionsWithCandidate.Except(_positionsToRemoveCandidate);
-
-            displayer.Mark(Enums.Color.Legal, positionsWithCandidate, _inputValue);
-            var toMarkIllegal = informer.WithCandidate(_positionsToRemoveCandidate, _inputValue);
-            displayer.Mark(Enums.Color.Illegal, toMarkIllegal, _inputValue);
-            displayer.HighlightBlock(positionsWithLegalCandidate.First());
-            displayer.HighlightHouse(positionsWithLegalCandidate.First(), _house);
+            displayer.Mark(Enums.Color.Legal, _positionsWithLegalCandidate, _inputValue);
+            displayer.Mark(Enums.Color.Illegal, _positionsWithIllegalCandidate, _inputValue);
+            displayer.HighlightBlock(Position);
+            displayer.HighlightHouse(Position, _house);
             displayer.SetValueFilter(_inputValue);
+
+            displayer.SetDescription(
+                $"In {_houseFormatted} value {_inputValue:D} can be placed only in {_blockFormatted}, "
+                + $"so any candidate {_inputValue:D} in {_blockFormatted} outside {_houseFormatted} can be removed.");
+        }
+
+        private void Explain1(Displayer displayer, Informer informer)
+        {
+            SetupDisplay(displayer, informer);
+
+            displayer.HighlightHouse(Position, _house);
+            displayer.SetValueFilter(InputValue.Empty);
+            displayer.SetDescription($"Lets focus on {_houseFormatted}...");
+        }
+        private void Explain2(Displayer displayer, Informer informer)
+        {
+            SetupDisplay(displayer, informer);
+
+            displayer.HighlightHouse(Position, _house);
+            displayer.SetValueFilter(_inputValue);
+            displayer.SetDescription($"... with filter we can see where value {_inputValue:D} can be placed...");
+        }
+        private void Explain3(Displayer displayer, Informer informer)
+        {
+            SetupDisplay(displayer, informer);
+
+            displayer.HighlightHouse(Position, _house);
+            displayer.MarkIfHasCandidate(Enums.Color.Legal, _positionsWithLegalCandidate, _inputValue);
+            displayer.SetValueFilter(_inputValue);
+            displayer.SetDescription($"... one of this cells has to be {_inputValue:D}, we don't know yet which one...");
+        }
+        private void Explain4(Displayer displayer, Informer informer)
+        {
+            SetupDisplay(displayer, informer);
+
+            displayer.HighlightBlock(Position);
+            displayer.MarkIfHasCandidate(Enums.Color.Legal, _positionsWithLegalCandidate, _inputValue);
+            displayer.SetValueFilter(_inputValue);
+            displayer.SetDescription($"... but we can notice that all this cells lay in {_blockFormatted}...");
+        }
+        private void Explain5(Displayer displayer, Informer informer)
+        {
+            SetupDisplay(displayer, informer);
+
+            displayer.HighlightHouse(Position, _house);
+            displayer.HighlightBlock(Position);
+            displayer.MarkIfHasCandidate(Enums.Color.Legal, _positionsWithLegalCandidate, _inputValue);
+            displayer.MarkIfHasCandidate(Enums.Color.Illegal, _positionsWithIllegalCandidate, _inputValue);
+            displayer.SetValueFilter(_inputValue);
+            displayer.SetDescription($"... it means we can remove candidate {_inputValue:D} from other cells in {_blockFormatted}.");
         }
 
         public override void Execute(Executor executor, Informer informer)
         {
             executor.RemoveCandidates(_inputValue, _positionsToRemoveCandidate);
+        }
+
+        public IEnumerable<Position> GetPositiosnWithIllegalCandidate(Informer informer)
+        {
+            return informer.WithCandidate(_positionsToRemoveCandidate, _inputValue);
         }
     }
 }
