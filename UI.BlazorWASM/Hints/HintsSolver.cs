27363c00c08265e0515063b429b5aa141dd80aea
Andżej <trader1999@o2.pl>
2020-06-27 22:18:46 +0200

    refact

--- UI.BlazorWASM/Hints/HintsSolver.cs
+++ UI.BlazorWASM/Hints/HintsSolver.cs
@@ -237,24 +237,16 @@ namespace UI.BlazorWASM.Hints
             {
                 return default;
             }
-
             if (values.Count == depth)
             {
-                if (positions.Any(pos => InputValue.NonEmpty.Except(values).Any(value => input.HasCandidate(pos, value))))
-                {
-                    return (positions, values);
-                }
-                else
-                {
-                    return default;
-                }
+                return positions.Any(pos => InputValue.NonEmpty.Except(values).Any(value => input.HasCandidate(pos, value)))
+                    ? (positions, values)
+                    : default;
             }
 
             foreach( var value in InputValue.NonEmpty.Except(values) )
             {
-                var positionsWithCandidate = house
-                    .Where(pos => input.HasCandidate(pos, value));
-
+                var positionsWithCandidate = house.Where(pos => input.HasCandidate(pos, value));
                 var count = positionsWithCandidate.Count();
                 if (count > depth || count == 0)
                 {
@@ -262,19 +254,14 @@ namespace UI.BlazorWASM.Hints
                 }
 
                 var positionsNew = positions.ToHashSet();
-                foreach( var pos in positionsWithCandidate )
-                {
-                    positionsNew.Add(pos);
-                }
-                var valuesNew = values.ToList();
-                valuesNew.Add(value);
+                positionsNew.UnionWith(positionsWithCandidate);
+                var valuesNew = new List<InputValue>(values) { value };
                 var result = HiddenSubsetStep(input, house, positionsNew, valuesNew, depth);
                 if ( result != default)
                 {
                     return result;
                 }
             }
-
             return default;
         }
         #endregion
