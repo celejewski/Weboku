a1f1efb12b9116b04d2123b30cfe4e769c752eef
Andżej <trader1999@o2.pl>
2020-07-18 08:42:36 +0200

    fixed bug

--- SmartSolver/TechniqueFinders/SkyscrapperFinder.cs
+++ SmartSolver/TechniqueFinders/SkyscrapperFinder.cs
@@ -29,19 +29,21 @@ namespace SmartSolver.TechniqueFinders
                     var possibleSkyscrappers = housesPaired
                         .Where(houses =>
                         houses.house1.Any(pos1 => houses.house2.Any(pos2 => pos1.x == pos2.x || pos1.y == pos2.y))
+                        && houses.house1.Any(pos1 => houses.house2.All(pos2 => !pos1.IsSharingHouseWith(pos2)))
                         && houses.house1.Concat(houses.house2).Select(pos => pos.block).Distinct().Count() == 4
                         );
 
                     foreach( var (house1, house2) in possibleSkyscrappers )
                     {
-                        var pos1 = house1.FirstOrDefault(pos1 => house2.All(pos2 => !pos1.IsSharingHouseWith(pos2)));
-                        var pos2 = house2.FirstOrDefault(pos2 => house1.All(pos1 => !pos1.IsSharingHouseWith(pos2)));
+                        var pos1 = house1.First(pos1 => house2.All(pos2 => !pos1.IsSharingHouseWith(pos2)));
+                        var pos2 = house2.First(pos2 => house1.All(pos1 => !pos2.IsSharingHouseWith(pos1)));
 
                         var positionsToRemoveFrom = Position.GetOtherPositionsSeenBy(pos1, pos2)
                             .Where(pos => grid.HasCandidate(pos, value));
 
                         var base1 = house1.First(pos => !pos.Equals(pos1));
                         var base2 = house2.First(pos => !pos.Equals(pos2));
+
                         yield return _factory.Skyscrapper(base1, base2, pos1, pos2, value);
                     }
                 }
