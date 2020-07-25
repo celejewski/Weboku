8cded8964e8fefc27e13b250932099728eab537b
Andżej <trader1999@o2.pl>
2020-07-25 13:57:05 +0200

    improve full house displayer

--- UI.BlazorWASM/Hints/SolvingTechniqueDisplayers/FullHouseDisplayer.cs
+++ UI.BlazorWASM/Hints/SolvingTechniqueDisplayers/FullHouseDisplayer.cs
@@ -1,7 +1,12 @@
 ﻿using Core.Data;
+using Microsoft.Extensions.Options;
+using Microsoft.Net.Http.Headers;
 using SmartSolver.SolvingTechniques;
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
+using YamlDotNet.Core.Tokens;
 
 namespace UI.BlazorWASM.Hints.SolvingTechniqueDisplayers
 {
@@ -10,15 +15,24 @@ namespace UI.BlazorWASM.Hints.SolvingTechniqueDisplayers
         private readonly Position _position;
         private readonly InputValue _value;
 
-        private House _house;
+        private readonly House _house;
+        private readonly IEnumerable<Position> _positionsInHouse;
         public FullHouseDisplayer(Informer informer, Displayer displayer, FullHouse fullHouse)
             : base(informer, displayer, fullHouse, "full-house")
         {
             _position = fullHouse.Position;
             _value = fullHouse.Value;
 
-            _explanationSteps.Add(Explain1);
+            _house = HintsHelper.HouseFirstOrDefault(_position,
+                positions => positions.Where(pos => !_informer.HasValue(pos))
+                                      .Count() == 1
+                );
 
+            _positionsInHouse = HintsHelper.GetPositionsInHouse(_position, _house);
+
+            _explanationSteps.Add(Explain01);
+            _explanationSteps.Add(Explain02);
+            _explanationSteps.Add(Explain03);
             for( int i = 0; i < _value - 1; i++ )
             {
                 var explain = ExplainN(i);
@@ -29,10 +43,6 @@ namespace UI.BlazorWASM.Hints.SolvingTechniqueDisplayers
 
         private void SetupDisplay()
         {
-            _house = HintsHelper.HouseFirstOrDefault(_position,
-                positions => positions.Where(pos => !_informer.HasValue(pos))
-                                      .Count() == 1
-                );
 
             _displayer.SetTitle(TitleKey);
             _displayer.HighlightHouse(_position, _house);
@@ -42,15 +52,35 @@ namespace UI.BlazorWASM.Hints.SolvingTechniqueDisplayers
             SetupDisplay();
 
             _displayer.SetDescription(DescriptionKey, _displayer.Format(_house, _position), _position);
-            _displayer.Mark(Enums.Color.Legal, _position, _value);
+            _displayer.MarkCells(Enums.Color.Illegal, _positionsInHouse);
+            _displayer.MarkCell(Enums.Color.Legal, _position);
             _displayer.SetValueFilter(_value);
         }
 
-        public void Explain1()
+        public void Explain01()
         {
             SetupDisplay();
 
-            _displayer.SetDescription(ExplanationKey("first"), _displayer.Format(_house, _position));
+            _displayer.SetDescription(ExplanationKey("01"), _displayer.Format(_house, _position));
+            _displayer.SetValueFilter(InputValue.Empty);
+        }
+
+        public void Explain02()
+        {
+            SetupDisplay();
+
+            _displayer.SetDescription(ExplanationKey("02"), _displayer.Format(_house, _position));
+            _displayer.MarkCell(Enums.Color.Legal, _position);
+            _displayer.SetValueFilter(InputValue.Empty);
+        }
+        public void Explain03()
+        {
+            SetupDisplay();
+
+            _displayer.SetDescription(ExplanationKey("03"), _displayer.Format(_house, _position));
+            _displayer.MarkCells(Enums.Color.Illegal, _positionsInHouse);
+            _displayer.MarkCell(Enums.Color.Legal, _position);
+            _displayer.SetValueFilter(InputValue.Empty);
         }
 
         public Action ExplainN(InputValue n)
@@ -58,14 +88,18 @@ namespace UI.BlazorWASM.Hints.SolvingTechniqueDisplayers
             return () =>
             {
                 SetupDisplay();
-                var positions = HintsHelper.GetPositionsInHouse(_position, _house);
                 var limit = n + 1;
                 for( int i = 0; i < limit; i++ )
                 {
-                    _displayer.MarkIfInputEquals(Enums.Color.Illegal, positions, i + 1);
+                    var value = i + 1;
+                    var pos = value != _value
+                    ? _positionsInHouse.First(pos => _informer.GetValue(pos) == value)
+                    : _position;
+                    _displayer.MarkCell(Enums.Color.Illegal, pos);
                 }
                 var digits = Enumerable.Range(0, limit).Select(i => (i + 1).ToString() + "... ");
                 _displayer.SetDescription(string.Join(" ", digits));
+                _displayer.SetValueFilter(limit);
             };
         }
 
@@ -73,7 +107,7 @@ namespace UI.BlazorWASM.Hints.SolvingTechniqueDisplayers
         {
             SetupDisplay();
             ExplainN(_value - 1)();
-            _displayer.Mark(Enums.Color.Legal, _position, _value);
+            _displayer.MarkCell(Enums.Color.Legal, _position);
             _displayer.SetDescription(ExplanationKey("last"), _displayer.Description, _value);
         }
     }
